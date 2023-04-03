11255512d8100d828b1b7a29d107dbd055196f0d
joshuamg41 <88290099+joshuamg41@users.noreply.github.com>
2023-04-03 15:49:34 -0400

    adding url http

--- config/env/produccion/server.js
+++ config/env/produccion/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
     proxy: true,
-    url: env('solemare.heroku.com'), // Sets the public URL of the application.
+    url: env('https://solemare.herokuapp.com/'), // Sets the public URL of the application.
     app: { 
       keys: env.array('APP_KEYS')
     },
